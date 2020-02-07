ea043311ea3a4b62d384ecc27b9912803ba1b5a2
Yunkai-Xiao <yunkai.xiao12@gmail.com>
2020-02-07 12:00:24 -0500

    change size

--- src/Canvas.js
+++ src/Canvas.js
@@ -25,8 +25,8 @@ function rand() {
 class Demo extends Component {
   state = {
     brushColor: rand(),
-    canvasHeight: 350,
-    canvasWidth: 380,
+    canvasHeight: 1000,
+    canvasWidth: 800,
     brushRadius: 4,
     lazyRadius: 8
   }
